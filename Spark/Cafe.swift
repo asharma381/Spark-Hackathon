061de53984eb632e839fce8a0f7f76e7289b42cb
Aditya Sharma <adityasharma1709@gmail.com>
2020-10-03 20:48:00 -0700

    Add files via upload

---
+++ Spark/Cafe.swift
@@ -0,0 +1,35 @@
+//
+//  Cafe.swift
+//  Cafegram2EN
+//
+//  Created by Farukh IQBAL on 06/06/2018.
+//  Copyright © 2018 Farukh IQBAL. All rights reserved.
+//
+
+import Foundation
+
+class Cafe {
+    var name: String
+    var type: String
+    var location: String
+    var image: String
+    var phone: String
+    var summary: String
+    var isVisited: Bool
+    var rating: String
+    
+    init(name: String, type: String, location: String, image: String, phone: String, summary: String, isVisited: Bool, rating: String = "") {
+        self.name = name
+        self.type = type
+        self.location = location
+        self.image = image
+        self.phone = phone
+        self.summary = summary
+        self.isVisited = isVisited
+        self.rating = rating
+    }
+    
+    convenience init() {
+        self.init(name: "", type: "", location: "", image: "", phone: "", summary: "", isVisited: false)
+    }
+}
